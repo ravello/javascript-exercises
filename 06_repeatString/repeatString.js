e305a2b20980ec3bce161cf6f1fa7baa4fd874f1
ravello <71367198+ravello@users.noreply.github.com>
2025-08-21 16:42:26 -0500

    finished the 6th exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    if (number < 0) return "ERROR";
 
+    let resultString = ""
+
+    for (let i = 0; i < number; i++) {
+        resultString += string;
+    }
+
+    return resultString;
 };
 
 // Do not edit below this line
