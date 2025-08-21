3f0bb412618bdf84cd8697217e943ad95daf9e73
ravello <71367198+ravello@users.noreply.github.com>
2025-08-21 17:04:11 -0500

    completed 7th exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    if (word.length < 1) return "";
 
+    let string = "";
+
+    for (let i = word.length-1; i >= 0; i--) {
+        string += word.at(i);
+    }
+
+    return string;
 };
 
 // Do not edit below this line
