a0c88e22e41c2429976961a1e6780cb27a3dfa0d
ravello <71367198+ravello@users.noreply.github.com>
2025-08-23 17:38:51 -0500

    finished 8 and 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (a < 1 || b < 1) return "ERROR";
 
+    let total = 0;
+
+    const larger = Math.max(a, b);
+    const smaller = Math.min(a, b);
+
+    for (let i = smaller; i <= larger; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
